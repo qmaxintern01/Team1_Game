8481eab36bc485047de8f2f72dd7942111257067
qmaxintern02 <qmaxintern02@gmail.com>
2026-08-25 14:02:12 +0900

    敵の攻撃とプレイヤーのHPの減完成

--- Assets/Scripts/Core/Health.cs
+++ Assets/Scripts/Core/Health.cs
@@ -5,18 +5,32 @@ namespace Team1
 {
     public class Health : MonoBehaviour, IDamageable
     {
+        // インスペクターで初期最大HPを設定できるように追加
+        [SerializeField] private int defaultMaxHp = 100;
+
         public event Action<int> OnDamaged;
         public event Action OnDied;
+        public event Action<int, int> OnHpChanged;
 
         public int MaxHp { get; private set; }
         public int CurrentHp { get; private set; }
         public bool IsDead => CurrentHp <= 0;
         public bool IsInvincible { get; set; }
 
+        private void Awake()
+        {
+            // 開始時にまだ初期化されていなければ、設定値で初期化する
+            if (MaxHp == 0)
+            {
+                Initialize(defaultMaxHp);
+            }
+        }
+
         public void Initialize(int maxHp)
         {
             MaxHp = maxHp;
             CurrentHp = maxHp;
+            OnHpChanged?.Invoke(CurrentHp, MaxHp);
         }
 
         public void TakeDamage(int amount)
@@ -28,6 +42,7 @@ namespace Team1
 
             CurrentHp = Mathf.Max(0, CurrentHp - amount);
             OnDamaged?.Invoke(amount);
+            OnHpChanged?.Invoke(CurrentHp, MaxHp);
 
             if (CurrentHp <= 0)
             {
